ae20e92f1e9b4c22bca8789614c4b80a063789f8
Salvatore <salvbarras@Salvatores-MacBook-Pro.local>
2025-07-13 00:04:38 +0200

    Update Home.tsx

--- tour-bud/src/components/Home.tsx
+++ tour-bud/src/components/Home.tsx
@@ -26,7 +26,7 @@ const Home: React.FC<HomeProps> = ({ userName, credits }) => {
   const mainCards = [
     {
       id: 'talk',
-      title: 'Create a Tour',
+      title: 'Create New Tour',
       subtitle: "Customized for your interests and location",
       icon: <Sparkles size={24} />,
       onClick: () => navigate('/location'),
